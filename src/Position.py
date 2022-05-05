70c682512c5488fe90665452c85c4dca718c0db5
Wojciech Kosztyła <wojtex1221@gmail.com>
2022-05-06 01:59:08 +0200

    [PROPER] Added compatibility with angles of 90 and 270 degrees to `angleToPoint`.

--- src/Position.py
+++ src/Position.py
@@ -14,11 +14,17 @@ class Position:
         return math.dist([self.x, self.y], [otherPosition.x, otherPosition.y])
 
     def angleToPoint(self, otherPoint):
-        tempAngle = math.atan((otherPoint.y - self.y) / (otherPoint.x - self.x))
-        if otherPoint.x < self.x:
-            return tempAngle + math.pi
-        else:
-            if otherPoint.y < self.y:
-                return tempAngle + 2 * math.pi
+        try:
+            tempAngle = math.atan((otherPoint.y - self.y) / (otherPoint.x - self.x))
+            if otherPoint.x < self.x:
+                return tempAngle + math.pi
             else:
-                return tempAngle
+                if otherPoint.y < self.y:
+                    return tempAngle + 2 * math.pi
+                else:
+                    return tempAngle
+        except ZeroDivisionError:
+            if otherPoint.y > self.y:
+                return math.pi /2
+            else:
+                return 3 * math.pi / 2
