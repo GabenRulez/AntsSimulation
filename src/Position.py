3907d900a486121094cd3bc565faad93545a0663
Wojciech Kosztyła <wojtex1221@gmail.com>
2022-05-05 23:29:11 +0200

    Fixed `angleToPoint` and `distanceToObject` to be "methods" rather than "functions".

--- src/Position.py
+++ src/Position.py
@@ -7,16 +7,16 @@ class Position:
         self.y = y
 
 
-def distanceToObject(self, otherPosition):
-    return math.dist([self.x, self.y], [otherPosition.x, otherPosition.y])
+    def distanceToObject(self, otherPosition):
+        return math.dist([self.x, self.y], [otherPosition.x, otherPosition.y])
 
 
-def angleToPoint(self, otherPoint):
-    tempAngle = math.atan((otherPoint.y - self.y)/(otherPoint.x - self.x))
-    if otherPoint.x < self.x:
-        return tempAngle + math.pi
-    else:
-        if otherPoint.y < self.y:
-            return tempAngle + 2*math.pi
+    def angleToPoint(self, otherPoint):
+        tempAngle = math.atan((otherPoint.y - self.y)/(otherPoint.x - self.x))
+        if otherPoint.x < self.x:
+            return tempAngle + math.pi
         else:
-            return tempAngle
+            if otherPoint.y < self.y:
+                return tempAngle + 2*math.pi
+            else:
+                return tempAngle
