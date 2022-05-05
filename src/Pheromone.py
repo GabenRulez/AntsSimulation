d25bd19415376a1bbd47172ca2d5c277447be33f
Wojciech Kosztyła <wojtex1221@gmail.com>
2022-05-06 01:57:42 +0200

    Added compatibility with angles of 90 and 270 degrees to `angleToPoint`.
    The lack of it was due to the use of `arctan` function, which doesn't have results for those.

--- src/Pheromone.py
+++ src/Pheromone.py
@@ -9,25 +9,26 @@ class Pheromone:
         position: Position,
         startingStrength: int = 100,
     ):
-        self.pheromoneType = pheromoneType
+        self.type = pheromoneType
         self.position = position
         self.strength = startingStrength
 
 
 def calculateAveragePheromonePosition(pheromones, trackedType):
-    if len(pheromones) == 0:
-        return None
-
     x_sum = 0
     y_sum = 0
     strength_sum = 0
 
     for pheromone in pheromones:
         if pheromone.type == trackedType:
-            x_sum += pheromone.x * pheromone.strength
-            y_sum += pheromone.y * pheromone.strength
+            x_sum += pheromone.position.x * pheromone.strength
+            y_sum += pheromone.position.y * pheromone.strength
             strength_sum += pheromone.strength
 
-    return Pheromone(
-        trackedType, Position(x_sum / strength_sum, y_sum / strength_sum), strength_sum
-    )
+    if strength_sum == 0:
+        return None
+
+    else:
+        return Pheromone(
+            trackedType, Position(x_sum / strength_sum, y_sum / strength_sum), strength_sum
+        )
