bc97f442e4e962a1b63d128b3830382263f30bce
Lint Action <lint-action@samuelmeuli.com>
2022-05-05 20:53:24 +0000

    Fix code style issues with Black

--- src/Position.py
+++ src/Position.py
@@ -1,12 +1,13 @@
 import math
 
+
 class Position:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def distanceToObject(self, otherPosition):
-        return math.dist([self.x, self.y],[otherPosition.x, otherPosition.y])
+        return math.dist([self.x, self.y], [otherPosition.x, otherPosition.y])
 
     def angleToPoint(self, otherPoint):
-        return math.atan((otherPoint.y - self.y)/(otherPoint.x - self.x))
+        return math.atan((otherPoint.y - self.y) / (otherPoint.x - self.x))
