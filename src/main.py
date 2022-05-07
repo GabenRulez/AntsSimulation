90c8239b1570e84d24ac9fe7568589e1265088d3
Aleksandra Cynk <aleksandra.cynk@ext.chime.com>
2022-05-07 22:23:32 +0200

    Add skeleton of side menu

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
-import random
-import pygame, emoji
+import pygame
+import pygame_textinput
 import WorldMap
 import Nest
 from Position import Position
 from Ant import Ant
 import numpy as np
 from PheromoneType import PheromoneType
-from enum import Enum
-import pygame.freetype  # Import the freetype module.
 
 
 mapColors = {
     "map": (102, 255, 179),
+    "menu": (102, 102, 102),
     "nest_a": (0, 128, 64),
     "nest_b": (40, 168, 104),
     "food": (127, 176, 105),
@@ -22,59 +21,63 @@ mapColors = {
 
 WIDTH, HEIGHT = 1920, 1080
 pygame.init()
-
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ants Simulation")
-# GAME_FONT = pygame.freetype.Font(24)
-pygame.font.init()  # you have to call this at the start,
-# if you want to use this module.
+
+#SIDE MENU
+pygame.font.init()
+GAME_FONT_LARGE = pygame.font.SysFont("arialunicode", 60)
 GAME_FONT = pygame.font.SysFont("arialunicode", 30)
+amountOfAntsInput = pygame_textinput.TextInputVisualizer()
 
 gameData = {
     "fps_limit": 60,
     "running": False,
     "clock": pygame.time.Clock(),
     "worldMap": WorldMap.WorldMap(0, 0),
-    "screenWidth": 1280,
-    "screenHeight": 720,
+    "mapWidth": 876,
+    "sideMenuWidth": 256,
+    "mapHeight": 720,
     "window": None,
 }
 
-
 def main():
     gameData["clock"] = pygame.time.Clock()
+
     setup()
+
 
     while gameData["running"]:
         loop()
+
 
     pygame.quit()
 
 
 def setup():
     gameData["running"] = True
-    worldMap = WorldMap.WorldMap(gameData["screenWidth"], gameData["screenHeight"])
+    worldMap = WorldMap.WorldMap(gameData["mapWidth"], gameData["mapHeight"])
     gameData["worldMap"] = worldMap
     gameData["antsNest"] = Nest.Nest(Position(100, 100), 50, worldMap)
 
     for _ in range(10):
         antSpawnPosition = Position(
-            np.random.uniform(low=0, high=gameData["screenWidth"]),
-            np.random.uniform(low=0, high=gameData["screenHeight"]),
+            np.random.uniform(low=0, high=gameData["mapWidth"]),
+            np.random.uniform(low=0, high=gameData["mapHeight"]),
         )
         worldMap.addAnt(Ant(antSpawnPosition, worldMap))
 
     worldMap.spawnFoodClump(Position(700, 300), 100)
 
     gameData["window"] = pygame.display.set_mode(
-        (gameData["screenWidth"], gameData["screenHeight"]), flags=pygame.RESIZABLE
+        (gameData["mapWidth"]+gameData["sideMenuWidth"], gameData["mapHeight"]), flags=pygame.RESIZABLE
     )
     updateScreenInfo()
 
-
 def loop():
     gameData["clock"].tick(gameData["fps_limit"])
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             gameData["running"] = False
 
@@ -87,9 +90,31 @@ def loop():
     drawFood()
     drawPheromones()
     drawAnts()
+    drawSideMenu(events)
 
     pygame.display.update()
 
+
+def drawSideMenu(events):
+    x_start = gameData["mapWidth"]
+    y_start = 0
+    pygame.draw.rect(
+        gameData["window"],
+        mapColors["menu"],
+        (x_start, y_start, x_start+gameData["sideMenuWidth"],y_start+ gameData["worldMap"].height)
+    )
+
+    MenuLabel = GAME_FONT_LARGE.render('MENU', True, (0,255,0))
+    WIN.blit(MenuLabel, (x_start+10, 10))
+
+    amountOfAntsLabel = GAME_FONT.render('Number of ants:', True, (0,255,0))
+    WIN.blit(amountOfAntsLabel, (x_start+10, 140))
+
+    amountOfAntsInput.update(events)
+    WIN.blit(amountOfAntsInput.surface, (x_start + 10, 200))
+
+    
+    
 
 def drawBackground():
     BACKGROUND_COLOR = (0, 0, 0)
@@ -108,7 +133,6 @@ def drawNest():
         if i % 2 == 0:
             color = mapColors["nest_a"]
         else:
-
             color = mapColors["nest_b"]
 
         pygame.draw.circle(
@@ -121,20 +145,12 @@ def drawNest():
 
 def drawFood():
     for food in gameData["worldMap"].foods:
-
-        # img1 = GAME_FONT.render("★", True, (r,g,b))
-        # WIN.blit(img1, (food.position.x, food.position.y))
-
         pygame.draw.circle(
             gameData["window"], mapColors["food"], (food.position.x, food.position.y), 2
         )
 
 
 def drawPheromones():
-    foodPheromonesColor = (63, 94, 49)
-    foodPheromonesColor = (255, 255, 0)
-    trailPheromonesColor = (29, 26, 5)
-
     for pheromone in gameData["worldMap"].pheromones:
         pheromoneStrength = min(pheromone.strength, 255)
         maxPheromoneStrength = 255
@@ -216,8 +232,8 @@ def drawAnts():
 
 def updateScreenInfo():
     screenInfo = pygame.display.Info()
-    gameData["screenWidth"] = screenInfo.current_w
-    gameData["screenHeight"] = screenInfo.current_h
+    # gameData["mapWidth"] = screenInfo.current_w
+    # gameData["mapHeight"] = screenInfo.current_h
 
 
 def updateAllAnts():
